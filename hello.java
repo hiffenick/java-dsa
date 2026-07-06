298821a418bf4d5b759468541ebb74796602a85d
Nikhil <kamthenikhil654@gmail.com>
2026-07-06 22:46:57 +0530

    Scanner DOne!

--- hello.java
+++ hello.java
@@ -1,8 +1,18 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class hello{
     public static void main(String[] args){
-      double a = 1.45;
-      System.out.println(a);
+
+      Scanner sc = new Scanner(System.in);
+
+      String name1,name2;
+      System.out.print("Enter name of Student :");
+      name1 = sc.nextLine();
+
+      System.out.print("Enter name of Student :");
+      name2 = sc.nextLine();
+
+      System.out.println("Name1 :"+name1+"\n"+"Name2 :"+name2);
+      sc.close();
     }
 }
